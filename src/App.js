b92de27084150b79e50c0be28812b51fc42a677c
vikas.peraka <vikasperaka@Vikass-MacBook-Air.local>
2018-07-03 20:24:51 -0500

    initial files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <p>Hello Veena</p>
+      </div>
+    );
+  }
+}
+
+export default App;
