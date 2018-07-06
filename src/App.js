820b1db1a2aa22512301bb63f64471c273d93ccc
“vikas.peraka” <vikas.peraka@gmail.com>
2018-07-05 22:17:40 -0500

    added nav and data for three years

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import { Nav, Navbar, NavItem } from 'react-bootstrap'
 // import 'node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      stats: []
+      stats: [],
+      players: []
     }
+    this.getStats2015 = this.getStats2015.bind(this);
+    this.getStats2016 = this.getStats2016.bind(this);
+    this.getStats2017 = this.getStats2017.bind(this);
     this.getStats = this.getStats.bind(this);
     this.includes = this.includes.bind(this);
   }
@@ -22,14 +27,96 @@ class App extends Component {
     return -1;
   }
 
-  getStats(event) {
-    this.setState({stats: []});
-    let players2016 = ['Jared Goff', 'Carson Wentz', 'Joey Bosa', 'Ezekiel Elliott', 'Jalen Ramsey', 'Ronnie Stanley', 'DeForest Buckner', 'Jack Conklin', 'Leonard Floyd', 'Eli Apple',
+  getStats2015() {
+    let players = [ 
+    'Jameis Winston', 
+'Marcus Mariota',
+'Dante Fowler', 
+'Amari Cooper', 
+'Brandon Scherff', 
+'Leonard Williams', 
+'Kevin White', 
+'Vic Beasley', 
+'Ereck Flowers', 
+'Todd Gurley', 
+'Trae Waynes', 
+'Danny Shelton', 
+'Andrus Peat', 
+'DeVante Parker', 
+'Melvin Gordon',
+'Kevin Johnson', 
+'Arik Armstead', 
+'Marcus Peters', 
+'Cameron Erving', 
+'Nelson Agholor', 
+'Cedric Ogbuehi', 
+'Bud Dupree', 
+'Shane Ray', 
+'D. J. Humphries', 
+'Shaq Thompson', 
+'Breshad Perriman', 
+'Byron Jones', 
+'Laken Tomlinson', 
+'Phillip Dorsett', 
+'Damarious Randall', 
+'Stephone Anthony', 
+'Malcom Brown' ];
+    this.setState({players: players})
+    this.getStats(players, 2015);
+  }
+
+  getStats2016() {
+    let players= ['Jared Goff', 'Carson Wentz', 'Joey Bosa', 'Ezekiel Elliott', 'Jalen Ramsey', 'Ronnie Stanley', 'DeForest Buckner', 'Jack Conklin', 'Leonard Floyd', 'Eli Apple',
     'Vernon Hargreaves', 'Sheldon Rankins', 'Laremy Tunsil', 'Karl Joseph', 'Corey Coleman', 'Taylor Decker', 'Keanu Neal', 'Ryan Kelly', '	Shaq Lawson', 'Darron Lee',
     'Will Fuller', 'Josh Doctson', 'Laquon Treadwell', 'William Jackson', 'Artie Burns', 'Paxton Lynch', 'Kenny Clark', 'Joshua Garnett', 'Robert Nkemdiche', '	Vernon Butler',
     'Germain Ifedi'];
+    this.setState({players: players})
+    this.getStats(players, 2016);
+  }
+
+  getStats2017() {
+    let players = [
+      'Myles Garrett', 
+'Mitchell Trubisky', 
+'Solomon Thomas', 
+'Leonard Fournette', 
+'Corey Davis', 
+'Jamal Adams', 
+'Mike Williams', 
+'Christian McCaffrey', 
+'John Ross', 
+'Patrick Mahomes', 
+'Marshon Lattimore',
+'Deshaun Watson', 
+'Haason Reddick', 
+'Derek Barnett', 
+'Malik Hooker', 
+'Marlon Humphrey', 
+'Jonathan Allen', 
+'Adoree\' Jackson', 
+'O. J. Howard', 
+'Garett Bolles', 
+'Jarrad Davis', 
+'Charles Harris', 
+'Evan Engram', 
+'Gareon Conley', 
+'Jabrill Peppers', 
+'Takkarist McKinley', 
+'Tre\'Davious White', 
+'Taco Charlton', 
+'David Njoku', 
+'T. J. Watt', 
+'Reuben Foster', 
+'Ryan Ramczyk', 
+    ];
+    this.setState({players: players})
+    this.getStats(players, 2017);
+  }
+
+  getStats(playersArr, time) {
+    this.setState({stats: []});
     let arr = [];
-    for (let year = 2016; year < 2018; year++){
+    for (let year = time; year < 2018; year++){
       fetch('http://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&season=' + year, {
         method: 'GET'
       }).then((req) => {
@@ -38,8 +125,8 @@ class App extends Component {
         let playerInfo = myJson.players;
         for(let i = 0; i < playerInfo.length; i++){
           if (playerInfo[i].esbid !== null) {
-            for (let j = 0; j < players2016.length; j++){
-              if (players2016[j].localeCompare(playerInfo[i].name) === 0){
+            for (let j = 0; j < playersArr.length; j++){
+              if (playersArr[j].localeCompare(playerInfo[i].name) === 0){
                 let inc = this.includes(arr, playerInfo[i].name);
                 if (inc !== -1){
                   arr[inc].Redraft += playerInfo[i].seasonPts;
@@ -68,10 +155,17 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Redraft</h1>
-        <button onClick={this.getStats}>Refresh</button>
+        {/* <button onClick={this.getStats}>Refresh</button> */}
         <div>
           <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"></link>
           <link rel="stylesheet" href="./dist/react-bootstrap-table.min.css"></link>
+          <Navbar>
+            <Nav>
+              <NavItem eventkey={1} onClick={this.getStats2015}>2015</NavItem>
+              <NavItem eventkey={2} onClick={this.getStats2016}>2016</NavItem>
+              <NavItem eventkey={3} onClick={this.getStats2017}>2017</NavItem>
+            </Nav>
+          </Navbar>
           <BootstrapTable data={this.state.stats}>
             <TableHeaderColumn dataField='Name' isKey={true} dataSort={true}>Name</TableHeaderColumn>
             <TableHeaderColumn dataField='Pick' dataSort={true}>Pick</TableHeaderColumn>
